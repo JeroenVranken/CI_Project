81175171cadd74caa68fbceef172df5aebd51f40
Jeroen Vranken <jeroen.vranken@xs4all.nl>
2017-11-09 13:46:40 +0100

    Files op goede plek

---
+++ torcs-server/torcs-client/SimpleRNN.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.autograd import Variable
+from torch import optim
+import numpy as np
+import math, random
+
+
+class SimpleRNN(torch.nn.Module):
+    def __init__(self, hidden_size):
+        super(SimpleRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.inp = torch.nn.Linear(22, hidden_size)
+        self.rnn = torch.nn.LSTM(hidden_size, hidden_size, 2, dropout=0.05)
+        self.out = torch.nn.Linear(hidden_size, 3)
+
+    def step(self, input, hidden=None):
+        #print(input.view(1, -1).unsqueeze(1))
+        input = self.inp(input.view(1, -1)).unsqueeze(1)
+        output, hidden = self.rnn(input, hidden)
+        output = self.out(output.squeeze(1))
+        return output, hidden
+
+    def forward(self, inputs, hidden=None, force=True, steps=0):
+        if force or steps == 0: steps = len(inputs)
+        outputs = Variable(torch.zeros(steps, 3, 1))
+        for i in range(steps):
+            if force or i == 0:
+                input = inputs[i]
+            else:
+                input = output
+            output, hidden = self.step(input, hidden)
+            outputs[i] = output
+        return outputs, hidden
+
+
